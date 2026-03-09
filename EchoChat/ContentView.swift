b6b2bf06247c845035b63d2f9efd05e7382e3235
Nguyen_Duy_Hung <hungnd@aipower.com.vn>
2026-03-09 10:29:53 +0700

    Initial Commit

---
+++ EchoChat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EchoChat
+//
+//  Created by Nguyen Duy Hung on 9/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EchoChat/EchoChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  EchoChatApp.swift
+//  EchoChat
+//
+//  Created by Nguyen Duy Hung on 9/3/26.
+//
+
+import SwiftUI
+
+@main
+struct EchoChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
